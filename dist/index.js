b8d2f1e0ae4829a43526df5e171fe9f15faf61c6
Reza <rezaaaa.z.1991@gmail.com>
2024-09-29 15:37:56 +0330

    ts1

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let user = [1, "reza"];
+user.push(44);
+console.log(user);
+//# sourceMappingURL=index.js.map
---
+++ src/ts1.ts
@@ -0,0 +1,61 @@
+// let sales = 123_456_789;
+// let course = "type script";
+// let is_published = true;
+
+///////////////////////////////////
+
+// function render(document:any) {
+//   console.log(document);
+// }
+
+////////////////////////////////////
+
+// let numbers: number[] = [];
+// let numbers1 = [5, 400, 4, 0];
+// numbers.forEach(n => n.)
+
+////////////////////////////////////
+
+//touple:
+
+// let user: [number, string] = [1, "reza"];
+// user.push(44);
+// console.log(user); //نقطه ی کور تایپ اسکریپت که به تاپل عنصر جدید اضافه میکند
+
+////////////////////////////////////
+
+//enum ts1 --> 57:41
+
+// const enum Size {
+//   Small = "s",
+//   Medium = "m",
+//   Large = "l",
+// }
+// let mySize: Size = Size.Large;
+// console.log(mySize);
+
+////////////////////////////////////
+
+// functions ts1 --> 1:03:18
+
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 50000) return income * 1.2;
+//   return income * 1.3;
+// }
+// calculateTax(10000);
+
+////////////////////////////////////////////////////
+
+// objects ts1 --> 1:14:46
+
+// let employee: {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// } = {
+//   id: 1,
+//   name: "reza",
+//   retire: (date: Date) => {
+//     console.log(date);
+//   },
+// };
